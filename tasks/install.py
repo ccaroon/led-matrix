443327a3b129cd839e28cd576d82f3ea01f8d4dd
Craig N. Caroon <craig.caroon@cengage.com>
2024-05-02 16:42:03 -0400

    * Change `requirements-cp.txt` into `project.yml` for each project * Added `install.project` invoke task - reads info from project.yml * Removed project specific invoke tasks in `install.`

--- tasks/install.py
+++ tasks/install.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-import zipfile
+import yaml
 
 from invoke import task
 
@@ -37,69 +37,12 @@ def file(ctx, file):
 
 
 @task(settings)
-def playground(ctx):
-    """ Install the Playground project """
-    src_code = {
-        "module": "playground",
-        "main": "playground/main.py",
-        "libs": [
-            "led_matrix.py"
-        ]
-    }
+def project(ctx, project_path):
+    """ Install the named Project and Dependencies """
+    with open(project_path, "r") as fptr:
+        project_data = yaml.safe_load(fptr)
 
-    util.install_project(src_code)
-
-
-@task(pre=[settings], aliases=["gol"])
-def game_of_life(ctx):
-    """ Install the Game Of Life project """
-
-    src_code = {
-        "module": "game_of_life",
-        "main": "game_of_life/main.py",
-        "libs": [
-            "led_matrix.py"
-        ]
-    }
-
-    util.install_project(src_code)
-
-
-@task(settings)
-def boids(ctx):
-    """ Install the Boids project """
-
-    src_code = {
-        "module": "boids",
-        "main": "boids/main.py",
-        "libs": [
-            "led_matrix.py"
-        ],
-        "ignore": [
-            "README.md"
-        ]
-    }
-
-    util.install_project(src_code)
-
-
-@task(settings)
-def info_panel(ctx):
-    """ Install the InfoPanel project """
-
-    src_code = {
-        "module": "info_panel",
-        "main": "info_panel/main.py",
-        "libs": [
-            "colors",
-            "aio.py",
-            "chronos.py",
-            "led_matrix.py",
-            "my_wifi.py"
-        ]
-    }
-
-    util.install_project(src_code)
+    util.install_project(ctx, project_data)
 
 
 @task
@@ -134,57 +77,3 @@ def view(ctx):
         ctx.run(f"tree {util.DEVICE_DEST}")
     else:
         ctx.run(f"ls -1FR {util.DEVICE_DEST}")
-
-
-
-BUNDLES = {
-    "Adafruit_CircuitPython_Bundle": {
-        "prefix": "adafruit-circuitpython-bundle",
-        "version": ("8", "20240501")
-    },
-    "CircuitPython_Community_Bundle": {
-        "prefix": "circuitpython-community-bundle",
-        "version": ("8", "20240502")
-    }
-}
-@task
-def requirements(ctx, reqs_file):
-    """ Install CircuitPython Requirements """
-    
-    # Make download dir
-    download_dir = f"{util.ROOT_DIR}/.circuit-python"
-    os.makedirs(download_dir, exist_ok=True)
-    
-    for bundle_name, bundle in BUNDLES.items():
-        prefix = bundle["prefix"]
-        cpy_version = bundle["version"][0]
-        bnd_version = bundle["version"][1]
-        # Download libs from cpy site if not exist
-        zip_file = f"{prefix}-{cpy_version}.x-mpy-{bnd_version}.zip"
-        if not os.path.exists(f"{download_dir}/{zip_file}"):
-            print(f"=> Downloading {zip_file}...")
-            with ctx.cd(download_dir):
-                ctx.run(f"wget -q https://github.com/adafruit/{bundle_name}/releases/download/{bnd_version}/{zip_file}")
-
-            # unzip
-            print(f"=> Extracting {zip_file}...")
-            with zipfile.ZipFile(f"{download_dir}/{zip_file}", 'r') as ziptr:
-                ziptr.extractall(path=download_dir)
-
-    # cp reqs to device
-    reqs = util.read_requirements(reqs_file)
-    for req in reqs:
-        bundle_name = os.path.dirname(req) or "Adafruit_CircuitPython_Bundle"
-        bundle = BUNDLES.get(bundle_name)
-        bundle_prefix = bundle["prefix"]
-        bundle_version = bundle["version"]
-        bundle_dir = f"{bundle_prefix}-{bundle_version[0]}.x-mpy-{bundle_version[1]}"
-        
-        pkg_name = os.path.basename(req)
-
-        src = f"{download_dir}/{bundle_dir}/lib/{pkg_name}"
-        dst = f"{util.DEVICE_DEST}/lib"
-        if os.path.isfile(src):
-            util.cp_if_newer(src, f"{dst}/{pkg_name}")
-        elif os.path.isdir(src):
-            util.cp_tree(src, dst)
--- tasks/util.py
+++ tasks/util.py
@@ -1,35 +1,51 @@
 import os
 import shutil
+import zipfile
 
 ROOT_DIR = os.path.abspath(f"{os.path.basename(__file__)}/..")
 
 USER = os.environ.get("USER")
 DEVICE_DEST = f"/media/{USER}/CIRCUITPY"
+# CircuitPython Library Bundles
+BUNDLES = {
+    "Adafruit_CircuitPython_Bundle": {
+        "prefix": "adafruit-circuitpython-bundle",
+        "version": ("8", "20240501")
+    },
+    "CircuitPython_Community_Bundle": {
+        "prefix": "circuitpython-community-bundle",
+        "version": ("8", "20240502")
+    }
+}
 
 
 # -- FOR TESTING --
 # DEVICE_DEST = f"/tmp/{USER}/CIRCUITPY"
 
 
-def install_project(src_code):
+def install_project(ctx, prj_data):
     # Install Module
     cp_tree(
-        src_code["module"],
+        prj_data["module"],
         f"{DEVICE_DEST}",
-        ignore=src_code.get("ignore", [])
+        ignore=prj_data.get("ignore", [])
     )
 
     # Install Main
-    cp_if_newer(src_code["main"], f"{DEVICE_DEST}/main.py")
+    cp_if_newer(prj_data["main"], f"{DEVICE_DEST}/main.py")
 
     # Install Libs
-    for file in src_code["libs"]:
+    for file in prj_data["libs"]:
         src = f"lib/{file}"
         dest_dir = f"{DEVICE_DEST}/lib"
         if os.path.isdir(src):
             cp_tree(src, dest_dir)
         else:
             cp_if_newer(src, f"{dest_dir}/{file}")
+
+    # Install requirements
+    if prj_data["requirements"]:
+        install_requirements(ctx, prj_data["requirements"])
 
 
 def cp_tree(src, dest, **kwargs):
@@ -85,13 +101,49 @@ def not_found(func, path, exc_info):
     print(f"=> {path} ... Nothing to do!")
 
 
-def read_requirements(file):
-    reqs = []
-    with open(file, "r") as fptr:
-        line = fptr.readline().strip()
-        while line:
-            if not line.startswith("#"):
-                reqs.append(line)
-            line = fptr.readline().strip()
-    return reqs
+# TODO: Allow `requirements` section to override CPY & Bundle Versions
+def install_requirements(ctx, reqs_list):
+    """ Install CircuitPython Requirements """
     
+    # Make download dir
+    download_dir = f"{ROOT_DIR}/.circuit-python"
+    os.makedirs(download_dir, exist_ok=True)
+    
+    for bundle_name, bundle in BUNDLES.items():
+        prefix = bundle["prefix"]
+        cpy_version = bundle["version"][0]
+        bnd_version = bundle["version"][1]
+        # Download libs from cpy site if not exist
+        zip_file = f"{prefix}-{cpy_version}.x-mpy-{bnd_version}.zip"
+        if not os.path.exists(f"{download_dir}/{zip_file}"):
+            print(f"=> Downloading {zip_file}...")
+            with ctx.cd(download_dir):
+                ctx.run(f"wget -q https://github.com/adafruit/{bundle_name}/releases/download/{bnd_version}/{zip_file}")
+
+            # unzip
+            print(f"=> Extracting {zip_file}...")
+            with zipfile.ZipFile(f"{download_dir}/{zip_file}", 'r') as ziptr:
+                ziptr.extractall(path=download_dir)
+
+    # cp reqs to device
+    for req in reqs_list:
+        bundle_name = os.path.dirname(req) or "Adafruit_CircuitPython_Bundle"
+        bundle = BUNDLES.get(bundle_name)
+        bundle_prefix = bundle["prefix"]
+        bundle_version = bundle["version"]
+        bundle_dir = f"{bundle_prefix}-{bundle_version[0]}.x-mpy-{bundle_version[1]}"
+        
+        pkg_name = os.path.basename(req)
+
+        src = f"{download_dir}/{bundle_dir}/lib/{pkg_name}"
+        dst = f"{DEVICE_DEST}/lib"
+        if os.path.isfile(src):
+            cp_if_newer(src, f"{dst}/{pkg_name}")
+        elif os.path.isdir(src):
+            cp_tree(src, dst)
+
+
+
+
+
+# 
